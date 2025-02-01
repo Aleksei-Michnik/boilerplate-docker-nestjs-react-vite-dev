6d863ab1996e1d530aa95ce4ac425f915ae7cc45
Aleksei Michnik <alexmich777@gmail.com>
2025-02-01 02:28:47 +0200

    Enable CORS for local development setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [`http://localhost:${process.env.VITE_PORT_HOST}`],
+    credentials: true,
+  });
+
   await app.listen(process.env.BACKEND_PORT ?? 3000);
 }
 bootstrap();
